e39c420340e37ccee3174e50abe1e6de17fd3e48
PezoMirko <accfireover@tutanota.com>
2021-05-22 11:59:28 +0200

    Get list of approvers

--- Wallet.sol
+++ Wallet.sol
@@ -3,9 +3,13 @@ pragma solidity 0.6.0;
 contract Wallet {
     address[] public approvers;
     uint public quorum;
-
+    
     constructor(address[] memory _approvers, uint _quorum) public {
         approvers = _approvers;
         quorum = _quorum;
+    }
+
+    function getApprovers() external view returns(address[] memory) {
+        return approvers; 
     }
 }
