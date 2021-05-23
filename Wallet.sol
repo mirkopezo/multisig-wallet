3d6b3d1038650342c4759c910e30c5ab575bf61f
PezoMirko <accfireover@tutanota.com>
2021-05-23 12:54:33 +0200

    Create transfers

--- Wallet.sol
+++ Wallet.sol
@@ -3,6 +3,17 @@ pragma solidity 0.6.0;
 contract Wallet {
     address[] public approvers;
     uint public quorum;
+    uint public nextId;
+
+    struct Transfer {
+        uint id;
+        uint amount; 
+        address payable to; 
+        uint approvals; 
+        bool sent; 
+    }
+
+    mapping(uint => Transfer) public transfers;
     
     constructor(address[] memory _approvers, uint _quorum) public {
         approvers = _approvers;
@@ -11,5 +22,16 @@ contract Wallet {
 
     function getApprovers() external view returns(address[] memory) {
         return approvers; 
+    }
+
+    function createTransfer(uint amount, address payable to) external {
+        transfers[nextId] = Transfer(
+            nextId, 
+            amount, 
+            to, 
+            0, 
+            false 
+        );
+        nextId++;
     }
 }
