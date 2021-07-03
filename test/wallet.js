d1dc6d1866dd54d637f0ef55740b5675bc9fde9a
Mirko Pezo <fireover@tutanota.com>
2021-07-03 16:35:14 +0200

    Preparing for test

---
+++ test/wallet.js
@@ -0,0 +1,13 @@
+const Wallet = artifacts.require('Wallet');
+
+contract('Wallet', (accounts) => {
+    let wallet;
+    beforeEach(async () => {
+        wallet = await Wallet.new(accounts[0], accounts[1], accounts[2], 2);
+        web3.eth.sendTransaction({
+            from: accounts[0],
+            to: wallet.address,
+            value: 1000
+        }); 
+    });
+});
