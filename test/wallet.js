769b82c6ef8ad0fd8fc4c673af1c178b9f6da3ff
Mirko Pezo <fireover@tutanota.com>
2021-07-03 23:03:46 +0200

    Test for list of  approvers and quorum

--- test/wallet.js
+++ test/wallet.js
@@ -3,11 +3,21 @@ const Wallet = artifacts.require('Wallet');
 contract('Wallet', (accounts) => {
     let wallet;
     beforeEach(async () => {
-        wallet = await Wallet.new(accounts[0], accounts[1], accounts[2], 2);
+        wallet = await Wallet.new([accounts[0], accounts[1], accounts[2]], 2);
         web3.eth.sendTransaction({
             from: accounts[0],
             to: wallet.address,
             value: 1000
         }); 
+    });
+
+    it('Should have correct approvers and quorum', async () => {
+        const approvers = await wallet.getApprovers();
+        const quorum = await wallet.quorum();
+        assert(approvers.length === 3);
+        assert(approvers[0] === accounts[0]);
+        assert(approvers[1] === accounts[1]);
+        assert(approvers[2] === accounts[2]); 
+        assert(quorum.toNumber() === 2);
     });
 });
