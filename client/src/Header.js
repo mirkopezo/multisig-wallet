879c7d224fabc345e63d3a4d1c3ae1a1c1431838
Mirko Pezo <fireover@tutanota.com>
2021-07-17 00:36:22 +0200

    Read smart contract data from the frontend with Web3

--- client/src/Header.js
+++ client/src/Header.js
@@ -1,9 +1,6 @@
 import React from 'react';
 
-// we wil define function, and pass data that we are interested in with props
 function Header({approvers, quorum}) {
-    // we will return html
-    // approvers is array so we need to join this into string. and after that we will render quroum
     return (
         <header>
             <ul>
