7fbb62f7ba901c1872b9ebfca9501e2085e4f83e
Mirko Pezo <fireover@tutanota.com>
2021-07-18 01:37:13 +0200

    Integrate Metamask

--- client/src/App.js
+++ client/src/App.js
@@ -13,7 +13,7 @@ function App() {
   const [transfers, setTransfers] = useState([]);
   useEffect(() => {
     const init = async () => {
-      const web3 = getWeb3();
+      const web3 = await getWeb3();
       const accounts = await web3.eth.getAccounts();
       const wallet = await getWallet(web3);
       const approvers = await wallet.methods.getApprovers().call();
--- client/src/utils.js
+++ client/src/utils.js
@@ -2,7 +2,23 @@ import Web3 from 'web3';
 import Wallet from './contracts/Wallet.json';
 
 const getWeb3 = () => {
-    return new Web3('http://localhost:9545');
+    return new Promise((resolve, reject) => {
+        window.addEventListener('load', async () => {
+            if(window.ethereum) {
+                const web3 = new Web3(window.ethereum);
+                try {
+                    await window.ethereum.enable();
+                    resolve(web3); 
+                } catch(error) {
+                    reject(error);
+                }
+            } else if(window.web3) {
+                resolve(window.web3);
+            } else {
+                reject('Must install Metamask');
+            }
+        });
+    });
 };
 
 const getWallet = async web3 => {
